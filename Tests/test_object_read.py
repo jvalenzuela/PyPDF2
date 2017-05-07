a5e022b956a86290804dcc9da1d49d395c86da37
Jason Valenzuela <jvalenzuela1977@gmail.com>
2017-05-06 21:25:41 -0400

    Add boolean object unit tests.

--- Tests/test_object_read.py
+++ Tests/test_object_read.py
@@ -39,3 +39,50 @@ class Null(unittest.TestCase):
         s = make_stream('NULL')
         with self.assertRaises(utils.PdfReadError):
             generic.NullObject.readFromStream(s)
+
+
+class Boolean(unittest.TestCase):
+    """Tests for reading a Boolean object."""
+    def test_type(self):
+        """Confirm the correct object type is created."""
+        s = make_stream('true')
+        o = generic.BooleanObject.readFromStream(s)
+        self.assertIsInstance(o, generic.BooleanObject)
+
+    def test_true_value(self):
+        """Confirm a true value is recorded as a True boolean object."""
+        s = make_stream('true')
+        o = generic.BooleanObject.readFromStream(s)
+        self.assertIs(o.value, True)
+
+    def test_false_value(self):
+        """Confirm a false value is recorded as a False boolean object."""
+        s = make_stream('false')
+        o = generic.BooleanObject.readFromStream(s)
+        self.assertIs(o.value, False)
+
+    def test_true_length(self):
+        """Confirm 4 bytes are read for a true value."""
+        s = make_stream('true')
+        generic.BooleanObject.readFromStream(s)
+        pos = s.tell()
+        self.assertEqual(pos, 4)
+
+    def test_false_length(self):
+        """Confirm 5 bytes are read for a false value."""
+        s = make_stream('false')
+        generic.BooleanObject.readFromStream(s)
+        pos = s.tell()
+        self.assertEqual(pos, 5)
+
+    def test_eof(self):
+        """Confirm an exception is raised if an EOF occurs in the input."""
+        s = make_stream('tru')
+        with self.assertRaises(utils.PdfReadError):
+            generic.BooleanObject.readFromStream(s)
+
+    def test_invalid_keyword(self):
+        """Confirm an exception is raised given an incorrect keyword."""
+        s = make_stream('TRUE')
+        with self.assertRaises(utils.PdfReadError):
+            generic.BooleanObject.readFromStream(s)
